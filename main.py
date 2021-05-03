15f6a281492f4caaf80c472ae497ebbfb3e206e5
Брит Серпинский <48598980+Vezono@users.noreply.github.com>
2021-05-03 17:21:40 +0300

    add tg user adding method

--- main.py
+++
@@ -1,8 +0,0 @@
-from bis import Client
-
-# client = Client('adsfghjyreLmkmnlgo93', 'https://bpl-id-system.herokuapp.com/')
-client = Client('koza', 'http://127.0.0.1:5000/')
-
-nick = 'Vezono4'
-print()
-print(client.get_user_by_id(client.get_user_by_nickname('Vezono2').id))
