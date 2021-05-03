0ee9e22943c8ed4ed99d430b543de03bd5b0da7a
vezono <vezonolab@gmail.com>
2021-05-03 17:24:56 +0300

    add setup file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bis-api-wrapper',
+    version='0.0.1',
+    author='Vezono',
+    author_email='vezonolab@gmail.com',
+    description='Wrapper for BIS',
+    long_description='Wrapper for BIS',
+    url='https://github.com/Vezono/bis-api-wrapper',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)
